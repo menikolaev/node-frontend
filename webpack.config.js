76ca24e91a4078e96debd2d15b56ed4a44fbe016
feretj <g.sergey.wolf@gmail.com>
2016-03-19 23:48:15 +0300

    Изменен webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  context: __dirname + "/app",
+  context: __dirname + '/app',
   entry: {
-    app: "./entry.js",
-    vendor: ["jquery", "bootstrap", "toastr", "magnific-popup"],
+    app: './entry.js',
+    vendor: ['jquery', 'bootstrap', 'toastr', 'magnific-popup'],
   },
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-    publicPath: '/static/'
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/static/',
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
   ],
   module: {
     loaders: [{
         test: /\.js$/,
-        include: __dirname + "/app",
-        loader: 'babel-loader'
+        include: __dirname + '/app',
+        loader: 'babel-loader',
       },
       {
         test: /\.json$/,
-        loader:'json-loader'
+        loader:'json-loader',
       },
       {
         test: /\.css$/,
@@ -31,18 +31,19 @@ module.exports = {
           'style',
           'css?modules&camelCase&localIdentName=[name]_[local]',
           'autoprefixer?browsers=last 5 version',
+
           // 'sass',
         ],
-      }
+      },
     ],
-    noParse: /validate\.js/
+    noParse: /validate\.js/,
   },
   node: {
     fs: 'empty',
     net: 'empty',
-    tls: 'empty'
+    tls: 'empty',
   },
   devServer: {
-    contentBase: __dirname + '/dist'
-  }
+    contentBase: __dirname + '/dist',
+  },
 };
